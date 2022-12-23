fee623bd8dd3ff1b35d912df3d90d75b543d17fd
johnnyMad-Dog <chrisbkarani@gmail.com>
2022-12-23 12:14:18 +0300

    Initial Commit

---
+++ RapptrChatApp/RapptrChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapptrChatAppApp.swift
+//  RapptrChatApp
+//
+//  Created by Chris Karani on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RapptrChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
